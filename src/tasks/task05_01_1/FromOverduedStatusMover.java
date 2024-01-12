5f2261330a27ad158100449525ff1596dc558676
Ali Kali <dankot92@gmail.com>
2024-01-12 19:54:03 +0600

    v5 fix

--- src/tasks/task05_01_1/FromOverduedStatusMover.java
+++ src/tasks/task05_01_1/FromOverduedStatusMover.java
@@ -3,7 +3,7 @@ package tasks.task05_01_1;
 public class FromOverduedStatusMover extends BookMover {
     protected void moveToStatus(Book book, Status requestedStatus) {
         switch (requestedStatus) {
-            case ARCHIVED -> {
+            case AVAILABLE -> {
                 book.setStatus(requestedStatus);
             }
             default ->
