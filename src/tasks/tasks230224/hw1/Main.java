d476100778e3defea1971a70d4ab3085a033469b
Russell Crowe <rasul.amirhan@gmail.com>
2024-02-26 16:08:26 +0600

    hw2 v3

--- src/tasks/tasks230224/hw1/Main.java
+++ src/tasks/tasks230224/hw1/Main.java
@@ -1,4 +1,5 @@
 package tasks.tasks230224.hw1;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,25 +12,38 @@ public class Main {
         }
         System.out.println("Все события корректны");
 
+
     }
 
     public static Movie[] getMovies() {
         return new Movie[]{
-                new Movie("Начало", 2010, 16),
-                new Movie("", 2010, 16)
+                new Movie("Начало", 2024, 16),
+                new Movie("Ч", 2024, 1)
 
         };
     }
 
     public static Theatre[] getTheatres() {
         return new Theatre[]{
-                new Theatre("Анна Каренина", 2017, 16)
+                new Theatre("Анна Каренина", 2023, 16)
         };
     }
 
     public static void validEvent(Event events) {
-        if (events == null || events.title == null || events.releaseYear == 0 || events.age == 0) {
-            throw new RuntimeException("Данные не найдены");
+        Calendar calendar = Calendar.getInstance();
+        int currentYear = calendar.getWeekYear();
+        if (events.title == null || events.title.isBlank()) {
+            throw new RuntimeException("Не указано название");
         }
+        if (events.releaseYear == 0 || events.releaseYear < currentYear) {
+            throw new RuntimeException("Не указан год. Год должен быть не позднее " + currentYear);
+        }
+        if (events.age == 0) {
+            throw new RuntimeException("Не указан возрастное ограничение");
+        }
+
     }
 }
+
+
+// events.title == null || events.releaseYear == 0 || events.age == 0
